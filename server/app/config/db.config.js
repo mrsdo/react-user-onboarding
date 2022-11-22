bdc112cb81e2f20383775989fe076171640a3f8f
Marti D <marti.dolce@29signals.org>
2022-11-22 17:02:47 -0500

    Adding Mongoose User Schema/Model with working Port 8080
    db_add_mongoose_user_schema_model

---
+++ server/app/config/db.config.js
@@ -0,0 +1,16 @@
+/**
+ * db.config.js
+ * Manages the configuration settings for connecting
+ * to MongoDB
+ *
+ * Defines the path for the .env file
+ * Initializes Mongoose models
+ * Creates connection to the process.env
+ *
+ *
+ */
+
+
+/**
+ * Define path to ./config/.env file
+ */
---
+++ server/app/models/user.model.js
@@ -0,0 +1,44 @@
+/**
+ * user.model.js
+ *
+ * Use Mongoose to build a User Schema
+ */
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    firstname: {
+        type: String,
+        required: true,
+        minlength: 3,
+        trim: true,
+    },
+    lastname: {
+        type: String,
+        required: true,
+        minlength: 3,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 6,
+        // use regex for further validation
+    },
+    role: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Role',
+        required: true,
+        default: 'user',
+        // index: true, TODO: What is schema role index?
+    },
+
+});
+
+module.exports = mongoose.model('User', UserSchema);
+
