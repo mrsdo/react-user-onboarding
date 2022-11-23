6071c1b72e7fba1bc8873175376ffaa22a6b5dce
Marti D <marti.dolce@29signals.org>
2022-11-22 20:44:46 -0500

    Added user authentication, verification
    Code source from bezcoder for managing tokens

---
+++ server/app/middleware/authJwt.js
@@ -0,0 +1,103 @@
+/**
+ * authJwt.js
+ * SRC: https://bezcoder.com
+ */
+
+/**
+ * To check if token is provided, legal or not. We get token from
+ * x-access-token of HTTP headers, then use jsonwebtoken verify() function
+ * @param {String} token
+ */
+
+
+const jwt = require("jsonwebtoken");
+const config = require("../config/auth.config.js");
+const db = require("../models");
+
+const User = db.user;
+const Role = db.role;
+
+verifyToken = (req, res, next) => {
+    let token = req.headers["x-access-token"];
+
+    if (!token) {
+        return res.status(403).send({ message: "No token provided!" });
+    }
+
+    jwt.verify(token, config.secret, db, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ message: "Unauthorized!" });
+        }
+        req.userId = decoded.id;
+        next();
+    });
+};
+
+isAdmin = (req, res, next) => {
+    User.findById(req.userId).exec((err, user) => {
+        if (err) {
+            res.status(500).send({ message: err });
+            return;
+        }
+
+        Role.find(
+            {
+                _id: { $in: user.roles }
+            },
+            (err, roles) => {
+                if (err) {
+                    res.status(500).send({ message: err });
+                    return;
+                }
+
+                for (let i = 0; i < roles.length; i++) {
+                    if (roles[i].name === "admin") {
+                        next();
+                        return;
+                    }
+                }
+
+                res.status(403).send({ message: "Require Admin Role!" });
+                next();
+            }
+        );
+    });
+};
+
+isModerator = (req, res, next) => {
+    User.findById(req.userId).exec((err, user) => {
+        if (err) {
+            res.status(500).send({ message: err });
+            return;
+        }
+
+        Role.find(
+            {
+                _id: { $in: user.roles }
+            },
+            (err, roles) => {
+                if (err) {
+                    res.status(500).send({ message: err });
+                    return;
+                }
+
+                for (let i = 0; i < roles.length; i++) {
+                    if (roles[i].name === "moderator") {
+                        next();
+                        return;
+                    }
+                }
+
+                res.status(403).send({ message: "Require Moderator Role!" });
+                next();
+            }
+        );
+    });
+};
+
+const authJwt = {
+    verifyToken,
+    isAdmin,
+    isModerator
+};
+module.exports = authJwt;
